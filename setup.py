0467a0cd9b1cad83f6c90674c10304a70b01aea8
Ayrton Teniente <ayrton.94.e@gmail.com>
2025-02-02 19:21:25 -0600

    Update PyPi version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ezpyc",
-    version="0.1.0",
+    version="0.1.1",
     author="Icebreaker",
     author_email="ayrton.94.e@gmail.com",
     description="Easy Python Commands",
