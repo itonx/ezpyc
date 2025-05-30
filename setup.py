8f6a1d35518f408c17f7fd49231c827f728eee9d
Ayrton Teniente <ayrton.94.e@gmail.com>
2025-05-30 01:09:37 -0600

    Add open command and update lib version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ezpyc",
-    version="0.1.1",
+    version="0.1.2",
     author="Icebreaker",
     author_email="ayrton.94.e@gmail.com",
     description="Easy Python Commands",
